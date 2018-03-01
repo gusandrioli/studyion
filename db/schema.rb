d7f99e62ec34f7539d8bef8596c2dc0c394e7339
gusandrioli <gusaandrioli@gmail.com>
2018-03-01 19:13:49 -0300

    db:rollback

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180301220029) do
+ActiveRecord::Schema.define(version: 20180301012735) do
 
   create_table "assignments", force: :cascade do |t|
     t.integer "user_id"
